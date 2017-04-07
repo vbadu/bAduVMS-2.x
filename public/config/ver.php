1494e4f0948a92b0f00efb20aa29a76fba45c1d4
jacochan <jacochan@outlook.com>
2017-04-07 10:55:51 +0800

    版本2.0.8.7 升级说明： 1、修复后台创建会员组时权限数组为空导致报错的bug； 2、删除httpd.ini，解决在虚拟主机下伪静态配置文件导致后台无法进入的bug； 3、如果修改了后台目录（admin）为其他名称如 guanli 等，请对应修改伪静态配置文件内容。具体方法，请访问论坛获取帮助； 4、修复捐赠物资无法提交的bug； 5、修复后台互助列表错别字、互助详情页条件判断存在bug。 升级方法：直接覆盖即可，本补丁包不涉及修改配置文件、数据库。

--- public/config/ver.php
+++
@@ -1,5 +0,0 @@
-<?php
-//版本
-$config['ver'] = '2.0.8.6'; //版本号
-$config['ver_name'] = 'bAduVMS'; //版本名称
-$config['ver_date'] = '20170321'; //版本时间
---
+++ system/config/ver.php
@@ -0,0 +1,5 @@
+<?php
+//版本
+$config['ver'] = '2.0.8.7'; //版本号
+$config['ver_name'] = 'bAduVMS'; //版本名称
+$config['ver_date'] = '20170407'; //版本时间
